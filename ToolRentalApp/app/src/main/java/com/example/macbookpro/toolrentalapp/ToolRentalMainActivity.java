7f7c4542038d1214749938f2af2943f7aa4d5438
Seth Donaldson <sethd63@gmail.com>
2016-08-15 19:46:04 -0600

    Begin Project

---
+++ ToolRentalApp/app/src/main/java/com/example/macbookpro/toolrentalapp/ToolRentalMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.macbookpro.toolrentalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToolRentalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_rental_main);
+    }
+}
